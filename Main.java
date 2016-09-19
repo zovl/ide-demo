a61681bd7d2a8a60ec1e31247e8f1056d263e006
zovl <849075906@qq.com>
2016-09-19 19:24:32 +0800

    2016-9-19 19:24:28

---
+++ Main.java
@@ -0,0 +1,9 @@
+package zovlzhongguanhua.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("java-idea-demo");
+    }
+}
---
+++ groovy-idea-gradle-demo/app/src/main/groovy/zovlzhongguanhua/demo/Main.groovy
@@ -0,0 +1,9 @@
+package zovlzhongguanhua.demo
+
+class Main {
+
+    static void main(args) {
+
+        println "groovy-idea-gradle-demo"
+    }
+}
